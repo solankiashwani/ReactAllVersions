4f30d31c07bb4537b0400075948782a13eb2f38e
Ashwani <ashwani@MacBook-Air.local>
2023-11-11 22:55:46 +0530

    Reactthreespring

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import Listgroup from "./components/ListGroup";
-import Alerts from "./components/Alerts";
-import Button from "./components/Button";
-import { useState } from "react";
+import React from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useDrag } from "@use-gesture/react";
+import { useSpring, a } from "@react-spring/three";
+
+function Obj() {
+  const { size, viewport } = useThree();
+  const aspect = size.width / viewport.width;
+  const [spring, set] = useSpring(() => ({
+    position: [0, 0, 0],
+    config: { mass: 1, friction: 40, tension: 800 },
+  }));
+  const bind = useDrag(({ movement: [x, y], down }) =>
+    set({
+      config: { mass: down ? 1 : 4, tension: down ? 2000 : 800 },
+      position: down ? [x / aspect, -y / aspect, 0] : [0, 0, 0],
+    })
+  );
+  return (
+    <a.mesh {...spring} {...bind()} castShadow>
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshNormalMaterial />
+    </a.mesh>
+  );
+}
 
 function App() {
-  let items = ["Delhi", "Pune", "Mathura"];
-  let listOfCities = "List of cities: Geronimo";
-  // This is how we pass function implementation as props in the element.
-  const handleItemSelect = (item: string) => {
-    console.log("Hello Geronimo: ", item);
-  };
-
-  const [alertVisible, alertVisibility] = useState(false);
   return (
-    <div>
-      {/* This is how we pass function props */}
-      <Listgroup
-        items={items}
-        heading={listOfCities}
-        onSelectItem={handleItemSelect}
+    <Canvas shadows camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={1} />
+      <spotLight
+        intensity={0.6}
+        position={[20, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        shadow-mapSize={[2048, 2048]}
+        castShadow
       />
-      {alertVisible && (
-        <Alerts>
-          Hello Everyone: Geronimo
-          <h1> There you go</h1>
-        </Alerts>
-      )}
-      <Button onClick={() => alertVisibility(!alertVisible)}>
-        Close alert
-      </Button>
-    </div>
+      <mesh receiveShadow position={[0, 0, -1]}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial color="springgreen" />
+      </mesh>
+      <Obj />
+    </Canvas>
   );
 }
 
