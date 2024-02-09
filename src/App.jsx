9dcec36a092f05b916a1188d83a843c6bfb54362
ashtense <solanki.ashwani@yahoo.com>
2024-02-09 14:19:45 +0530

    First changes to game

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,10 @@
-import React, { useState } from 'react';
-import Listgroup from "./components/ListGroup";
-import Alerts from "./components/Alerts";
-import Button from "./components/Button";
-import FormWithDynamicFields from "./components/FormWithDynamicFields";
+import React from 'react'
+import Square from './components/Square'
 
-let items = ["Delhi", "Pune", "Mathura"];
-let listOfCities = "List of cities: Geronimo";
-// This is how we pass function implementation as props in the element.
-const handleItemSelect = (item) => {
-  console.log("Hello Geronimo: ", item);
-};
-
-const App = () => {
-  const [alertVisible, alertVisibility] = useState(true);
-  const [alertChangeCount, setAlertChangeCount] = useState(0);
-
-  const toggleAlertVisibility = () => {
-    alertVisibility(!alertVisible);
-    setAlertChangeCount(alertChangeCount + 1);
-  };
-
+export default function App() {
   return (
     <div>
-      {/* <Header /> */}
-      {/* This is how we pass function props */}
-      <Listgroup
-        items={items}
-        heading={listOfCities}
-        onSelectItem={handleItemSelect}
-      />
-      {alertVisible && (
-        <Alerts>
-          Hello Everyone: Geronimo
-          <h1> There you go</h1>
-        </Alerts>
-      )}
-      <Button onClick={toggleAlertVisibility}>
-        Close alert
-      </Button>
-      <p>Alert visibility changed {alertChangeCount} times</p>
-      <FormWithDynamicFields />
-      {/* <Footer /> */}
+      <Square />
     </div>
-  );
+  )
 }
-
-export default App;
